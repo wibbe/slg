2674bb6e4c995305e45dbd5e3c3fc7c69d0ef0fe
Daniel Wiberg <wibbe@blueeye.se>
2012-04-26 20:12:11 +0200

    Started to implement some font rendering

---
+++ src/Font.cpp
@@ -0,0 +1,20 @@
+
+#include "slg/Font.hpp"
+
+#include <stdlib.h>
+
+#define STB_TRUETYPE_IMPLEMENTATION
+#include "stb_truetype.h"
+
+namespace slg {
+  
+  Font::Font(const char * font, int size)
+  {
+  }
+  
+  Font::~Font()
+  {
+  }
+  
+  
+}
